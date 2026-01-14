405d0a3cc4bbadc782f094c861c06d613d249e9b
Emmaxyart <emmanueledobor34@gmail.com>
2026-01-15 00:28:29 +0100

    made fixes to the heading text

--- app/page.tsx
+++ app/page.tsx
@@ -6,8 +6,8 @@ import { FeaturedListings } from "@/components/featured-listings"
 export default function Dashboard() {
   return (
     <div className="min-h-screen bg-background">
-      <main className="px-4 sm:px-12 py-4 bg-[#F9FAFB] sm:py-6">
-        <h1 className="text-2xl sm:text-3xl text-[#191919] font-semibold mb-6 sm:mb-6 text-Type-face">Welcome, Ahmed</h1>
+      <main className="px-4 sm:px-12 bg-[#F9FAFB] py-4">
+        <h1 className="text-[20px] sm:text-[20px] text-[#191919] font-semibold mb-6 sm:mb-6 text-Type-face">Welcome, Ahmed</h1>
 
         <div className="grid grid-cols-1 md:grid-cols-3 gap-4 sm:gap-6 mb-6">
           <div className="md:col-span-2">
